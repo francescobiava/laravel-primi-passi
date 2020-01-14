555522e31bcd518ecf21fac2309560c1303331db
Francesco <franc.ns@gmail.com>
2020-01-14 15:39:48 +0100

    added basic layout

--- resources/views/cds.blade.php
+++ resources/views/cds.blade.php
@@ -1,31 +1,18 @@
-<!DOCTYPE html>
-<html lang="en">
+@extends('layouts.basicLayout')
 
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="style.css">
-    <link href="https://fonts.googleapis.com/css?family=Lato&display=swap" rel="stylesheet">
-    <title>Php Dischi</title>
-</head>
+@section('content')
+    <header>
+        <img src="http://pluspng.com/img-png/spotify-logo-png-open-2000.png" alt="">
+    </header>
 
-<body>
-    <div class="container">
-        <header>
-            <img src="http://pluspng.com/img-png/spotify-logo-png-open-2000.png" alt="">
-        </header>
-
-        <div class="cds-container">
-            @foreach ($cds as $cd)
-                <div class="cd">
-                    <img src="{{ $cd['poster'] }}" />
-                    <h3>{{ $cd['title'] }}</h3>
-                    <small>{{ $cd['author'] }}</small>
-                    <strong>{{ $cd['year'] }}</strong>
-                </div>
-            @endforeach
-        </div>
+    <div class="cds-container">
+        @foreach ($cds as $cd)
+            <div class="cd">
+                <img src="{{ $cd['poster'] }}" />
+                <h3>{{ $cd['title'] }}</h3>
+                <small>{{ $cd['author'] }}</small>
+                <strong>{{ $cd['year'] }}</strong>
+            </div>
+        @endforeach
     </div>
-</body>
-
-</html>
+@endsection
